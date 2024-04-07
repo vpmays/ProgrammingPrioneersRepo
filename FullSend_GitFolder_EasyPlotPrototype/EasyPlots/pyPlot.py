c1602e8d1b3dc5bdb27bead0eacea23b350d87f8
vincentmays006 <104940949+vincentmays006@users.noreply.github.com>
2024-04-07 15:56:58 -0600

    added csv upload page, but this page isn't functional yet

--- FullSend_GitFolder_EasyPlotPrototype/EasyPlots/pyPlot.py
+++ FullSend_GitFolder_EasyPlotPrototype/EasyPlots/pyPlot.py
@@ -4,39 +4,33 @@ from matplotlib import pyplot as plt
 import numpy as np 
 
 
-def pyPlot1(freq=1):
-    t = np.arange(0.0, 2.0, 0.1)
-    s = 1 + np.sin(float(freq)*np.pi*t)
-
-    fig, ax = plt.subplots()
-    ax.plot(t,s)
-    ax.set(xlabel="time (s)", ylabel="voltage (mV)",
-            title='Made with matplotlib')
-    ax.grid()
-
+def pyPlot1(xdata=None, ydata=None, plotTitle='Main', xaxisTitle='x-axis', yaxisTitle='y-axis'):
+    if xdata == None:
+        xdata = ['default', 'defaul', 'default']
+    if ydata == None:
+        ydata = [3,2,1]
+    
+    fig = plt.figure()
+    plt.bar(xdata, ydata)
+    plt.xlabel(xaxisTitle)
+    plt.ylabel(yaxisTitle)
+    plt.title(plotTitle)
+    
     fig.savefig('EasyPlots/static/pyPlot.png')
 
 
-def pyPlot2(value=1):
-    '''
-    [8,4,3,2,1,1]
-    data_canada = px.data.gapminder().query("country == 'Canada'")
-    fig = px.bar(data_canada, x='year', y='pop')
-
-    output_html_path=r"EasyPlots/templates/pyPlot.html"
-    input_template_path = r"EasyPlots/templates/home.html"
-
-    plotly_jinja_data = {"fig":fig.to_html(full_html=False)}
-    #consider also defining the include_plotlyjs parameter to point to an external Plotly.js as described above
-
-    with open(output_html_path, "w", encoding="utf-8") as output_file:
-        with open(input_template_path) as template_file:
-            j2_template = Template(template_file.read())
-            output_file.write(j2_template.render(plotly_jinja_data))
-    '''
-    experience_dict = {'Programming Language': ['Excel', 'Python', 'Tableau', 'R', 'Bash', 'PowerShell'], 'Years of Experience (As of April 2022)': [float(value),4,3,2,1,1]}
-
-    fig = px.bar(experience_dict, x='Programming Language', y='Years of Experience (As of April 2022)')
+def pyPlot2(xdata=None, ydata=None, plotTitle='Main', xaxisTitle='x-axis', yaxisTitle='y-axis'):
+    
+    if xdata == None:
+        xdata = ['default', 'defaul', 'default']
+    if ydata == None:
+        ydata = [3,2,1]
+    
+    experience_dict = {}
+    experience_dict[xaxisTitle] = xdata
+    experience_dict[yaxisTitle] = ydata
+    
+    fig = px.bar(experience_dict, x=xaxisTitle, y=yaxisTitle, title=plotTitle)
 
     fig.write_html("EasyPlots/templates/pyPlot.html")
 
--- FullSend_GitFolder_EasyPlotPrototype/EasyPlots/views.py
+++ FullSend_GitFolder_EasyPlotPrototype/EasyPlots/views.py
@@ -1,11 +1,15 @@
-from flask import Blueprint, render_template, request, flash, jsonify #import needed modules from flask
+from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for #import needed modules from flask
 from flask_login import login_required, current_user #anything on the user table can be accessed with current_user
 from .models import Note
 from . import db
 import json
 from .pyPlot import pyPlot1, pyPlot2
+import os
 
 views = Blueprint('views', __name__) # intialize Blueprint("nmae of blueprint", __name__) and store it as "views" object
+plotTitle = '' #put this out here so I could access it in the pyPlot route
+
+
 
 # @ is how you define a view/route. format is @nameOfBlueprint.route("url to get to this endpoint", methods = [the methods you will alow])
 @views.route('/', methods=['GET', 'POST']) #defining homepage so route is just '/', and we need to allow GET and POST methods on this page
@@ -14,10 +18,24 @@ views = Blueprint('views', __name__) # intialize Blueprint("nmae of blueprint", 
 def home(): #when you hit route '/' (homepage) this function is called
     #if method is 'POST' when you hit the home page, get the note and check it, then store it in db
     if request.method == 'POST':
+        #get data
+        xdata = request.form.get('xdata').split(',') #data comes back as string for now, so need ot split it by ','
+        ydata_1 = request.form.get('ydata').split(',') #data comes back as string for now, so need ot split it by ','
+
+        #cinvert numbers in string for y-axis to float values
+        ydata = []
+        for num in ydata_1:
+            ydata.append(float(num))
+        #store plot titles
+        plotTitle = request.form.get('plotTitle')
+        xaxisTitle = request.form.get('xaxisTitle')
+        yaxisTitle = request.form.get('yaxisTitle')
+        ''''
         note = request.form.get('note')
         if len(note) < 1:
             flash('Note must be at least 1 character.', category='error' )
         else:
+            
             new_note = Note(data=note, user_id=current_user.id)
             #store note in db
             db.session.add(new_note)
@@ -25,13 +43,33 @@ def home(): #when you hit route '/' (homepage) this function is called
             #notify user that note was stored
             flash('Note added!', category='success')
             #using date from note to plug into pyPlots. This is where we will pull in data from template plot form
+            
             pyPlot1(note)
             pyPlot2(note)
             #render/display the html page home.html with user set to current_user, 
             #   must be in folder named templates to find it with this function,
             #   can add any variables we want (in this case we created a 'user' variable) which we can then access in the 
             #   hmtl file.
+            '''
+        pyPlot1(xdata, ydata, plotTitle, xaxisTitle, yaxisTitle)
+        pyPlot2(xdata, ydata, plotTitle, xaxisTitle, yaxisTitle)
     return render_template("home.html", user=current_user)
+
+@views.route('/upload', methods=['GET', 'POST']) #defining upload page with route '/upload', we need to allow GET and POST methods on this page
+@login_required #require the user to be logged in to get to this page
+def upload():
+    
+    #check if it's get or post request
+    if request.method == 'POST':
+        #capture uploaded csv
+        uploaded_csv = request.files['file']
+        #check if a csv was submitted
+        if uploaded_csv.filename != '':
+            uploaded_csv.save('EasyPlots/static/' + uploaded_csv.filename)
+
+            return redirect(url_for('views.home', user=current_user))
+    
+    return render_template("upload.html", user=current_user)
 
 #route for deleting notes. I'm thinking we won't need this for our code
 @views.route('/delete-note', methods=['POST'])
